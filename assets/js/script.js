b52157529758071c4147f2a892b25c7675b28f7e
Genius001 <stv_aditya@yahoo.com>
2024-08-02 15:58:00 +0700

    belajar JS

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+//variable
+//1. var = Old School
+//2. let = ES6
+//3. const = ES6
+
+var x = 0;
+x = 15
+console.log(x);
+
+//variable modern
+//1. let
+// let 0 = tidak boleh pakai angka
+let z = 10;
+z = 5;
+let _a = 10
+let $a = 10
+let isCarBroken = true //best Practice
+// let *a = 10 //tidak boleh diawali simbol selain 
+console.log(z);
+
+//2. const
+const y = 10;
+const COLOR_RED = 'red'; //best Practice
+console.log(y)
